657381d15aff8f0b63c235ec336c455eed806e9b
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-04-08 01:04:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-holiday_hash.collect do |holiday, supply|
+holiday_hash[:winter].collect do |holiday, supply|
+  supply
 end
 end
 
@@ -59,7 +60,6 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-
 
   end
 end
