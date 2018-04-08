7273b90d24d3ff5e926701e0f9a8328069027717
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-04-08 00:52:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,6 +70,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supply|
       holiday if supply.include?("BBQ")
+      return holiday
     end
   end
 end
